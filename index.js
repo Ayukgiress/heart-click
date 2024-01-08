09c8fdd4dad9aada89af79d850eb675befc28e90
AyukGiress <giress865@gmail.com>
2024-01-08 04:56:04 +0100

    my proje

--- index.js
+++ index.js
@@ -3,36 +3,35 @@ const times = document.querySelector('#times')
 
 let clickTime = 0
 
-loveMe.addEventListener('click', (e) => {
-   if(clickTime === 0) {
-      clickTime = new Date().getTime()
-   } else {
-    if((new Date().getTime() - clickTime) < 800) {
-    createHeart(e)
-    clickTime = 0
+loveMe.addEventListener("click", (e) => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime()
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      createHeart(e)
+      clickTime = 0
     } else {
-        clickTime = new Date().getTime()
+      clickTime = new Date().getTime()
     }
-   }
-})
+  }
+});
 
 const createHeart = (e) => {
-const heart = document.createElement('i')
-heart.classList.add('fas')
-heart.classList.add('fa-heart')
+  const heart = document.createElement('i')
+  heart.classList.add('fas')
+  heart.classList.add('fa-heart')
 
-const x = e.clientX
-const y = e.clientY
+  const x = e.clientX
+  const y = e.clientY
 
+  const leftOffset = e.target.offsetLeft
+  const topOffset = e.target.offsetTop
 
-const leftOffset = e.target.offsetLeft
-const topOffset = e.target.offsetTop
+  const xInside = x - leftOffset
+  const yInside = y - leftOffset
 
-const xInside = x - leftOffset
-const yInside = y - leftOffset
+  heart.style.top = `${yInside}px`
+  heart.style.left = `${xInside}px`
 
-heart.style.top = `${yInside}px`
-heart.style.left = `${xInside}px`
-
-loveMe.appendChild(heart)
+  loveMe.appendChild(heart)
 }
